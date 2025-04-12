91c045874cc0c751ecca9952121f57ba6aec6706
novlan1 <1576271227@qq.com>
2025-04-12 10:16:41 +0800

    docs: update docs config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -14,25 +14,25 @@ const base = env.VITE_DOCS_PUBLISH_PATH || '/'; // '/press-components/';
 console.log('base', base)
 
 return {
-  // lang: 'zh-CN',
-  // title: 'press-components',
-  // description: '',
-  // appearance: false,
-  // base,
-  // head: [
-  //   [
-  //     'link',
-  //     {
-  //       rel: 'icon',
-  //       href: '/images/press-ui-favicon-888.ico'
-  //     }
-  //   ]
-  // ],
-  // themeConfig: {
-    // logo: 'https://mike-1255355338.cos.ap-guangzhou.myqcloud.com/press%2Fimg%2Fpress-ui-avatar-transparent.png',
-  //   nav,
-  //   sidebar,
-  //   footer
-  // }
+  lang: 'zh-CN',
+  title: 'press-components',
+  description: '',
+  appearance: false,
+  base,
+  head: [
+    [
+      'link',
+      {
+        rel: 'icon',
+        href: '/images/press-ui-favicon-888.ico'
+      }
+    ]
+  ],
+  themeConfig: {
+    logo: 'https://mike-1255355338.cos.ap-guangzhou.myqcloud.com/press%2Fimg%2Fpress-ui-avatar-transparent.png',
+    nav,
+    sidebar,
+    footer
+  }
 }
 })

8e098ac6dcd08f96790448a772332abcf5c15e74
novlan1 <1576271227@qq.com>
2025-04-12 15:40:58 +0800

    docs: update docs

--- packages/tdesign-vue-next/demo/main.ts
+++ packages/tdesign-vue-next/demo/main.ts
@@ -1,9 +1,15 @@
 import { createApp } from 'vue';
+import TDesign from 'tdesign-vue-next';
+
 import App from './App.vue';
 import router from './router';
+import 'tdesign-vue-next/es/style/index.css';
+
 
 const app = createApp(App);
 
 app.use(router);
+app.use(TDesign);
+
 
 app.mount('#app');

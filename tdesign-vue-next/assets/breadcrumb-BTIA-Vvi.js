f61fe6ce9d9852e3edbc257f6648a7999991aef2
novlan1 <1576271227@qq.com>
2025-04-24 17:15:09 +0000

    Deploying to docs from  @ f82cac5229d8c180b43e768f42559d8b98e04145 🚀

---
+++ tdesign-vue-next/assets/breadcrumb-BTIA-Vvi.js
@@ -0,0 +1,1 @@
+import{B as e}from"./index-7F3NSlh1.js";import{d as r,a,u as o,o as t}from"./index-ChvbdMNo.js";const u=r({__name:"breadcrumb",setup(c){return(n,s)=>(t(),a(o(e)))}});export{u as default};

cc5c45d0e97bb9b2d12803c5033449a1ca4b681f
novlan1 <1576271227@qq.com>
2025-05-06 01:05:12 +0800

    feat: add publish script

---
+++ scripts/release/publish.sh
@@ -0,0 +1,7 @@
+pnpm --filter="./packages/shared" publish --no-git-checks
+
+pnpm --filter="./packages/press-ui-next" publish --no-git-checks
+pnpm --filter="./packages/swiper-next" publish --no-git-checks
+
+pnpm --filter="./packages/element-plus" publish --no-git-checks
+pnpm --filter="./packages/tdesign-vue-next" publish --no-git-checks
